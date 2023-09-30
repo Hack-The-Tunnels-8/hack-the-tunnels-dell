81c7d31021d00a9ed3bdd12d2a65fb1a3b376ab4
Pray <preynarayan@cmail.carleton.ca>
2023-09-30 12:48:00 -0400

    added hello world for issue #1 service

--- service/src/infrastructure/api/router.ts
+++ service/src/infrastructure/api/router.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const getAPIRoot = async (_: Request, response: Response) => {
   response.json({
-    message: "API - 👋",
+    message: "Hello World",
   });
 };
 
