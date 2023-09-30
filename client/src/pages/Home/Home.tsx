13ed8b374771b06ebec90dc768f1772828ad5d13
TEMtheLEM <conninch@pm.me>
2023-09-30 12:13:27 -0400

    Issue 2 attempt 1

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -20,6 +20,7 @@ function Home() {
     <Page>
       <div className="home-page">
         <h1 className="home-page__title">Home</h1>
+        <p>Hello, World!</p>
         <h2>Products:</h2>
         <div className="home-page__products">
           {products.map((product) => (
